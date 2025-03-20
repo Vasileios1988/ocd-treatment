7ae196612b57c1ffc7418dcda5e8f808d51d942c
Vasileios Milias <v.milias1@gmail.com>
2025-03-20 20:57:45 +0000

    Added Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+
+    return (
+        <div>
+            <h1> Welcome to OCD Treatment Centre</h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl odio, tincidunt ac nibh quis, faucibus luctus enim. Pellentesque fringilla eget leo ac faucibus.  </p>
+        </div>
+    );
+}
