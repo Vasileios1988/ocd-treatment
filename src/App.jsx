a4b967f092e2c4fcd3097caa9bbc935d5e796602
Vasileios Milias <v.milias1@gmail.com>
2025-03-20 22:40:16 +0000

    Installed Tailwind and initialized configuration

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-
+      <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-3xl font-bold">
+        Tailwind is working! 🎉
+      </div>
     </>
   )
 }
